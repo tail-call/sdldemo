3c053974701702796dd5930651b7b026a8bae29f
Maria Zaitseva <maria@tail-call.ru>
2023-12-21 23:22:13 +0700

    Whoops, I forgot to commit the files

--- SdlDemo/App.swift
+++ SdlDemo/App.swift
@@ -5,4 +5,38 @@
 //  Created by Maria Zaitseva on 21.12.2023.
 //
 
-import Foundation
+import SDL
+
+class App {
+    let display: Display
+
+    init(display: Display) {
+        self.display = display
+    }
+
+    func run() {
+        var quit = false
+        var event = SDL_Event()
+
+        while !quit {
+            while SDL_PollEvent(&event) > 0 {
+                Log.info("event", event.type)
+                if event.type == SDL_QUIT.rawValue {
+                    quit = true
+                }
+            }
+
+            display.draw { context in
+                context.setClearColor(red: 1, green: 1, blue: 1, alpha: 1)
+                context.clear()
+
+                context.setDrawColor(red: 0, green: 0, blue: 0, alpha: 1)
+                context.fill(rect: Rect(x: 20, y: 20, w: 20, h: 20))
+            }
+
+            SDL_Delay(100)
+        }
+
+        SDL_Quit()
+    }
+}
--- SdlDemo/Log.swift
+++ SdlDemo/Log.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+enum Log {
+    static func info(_ xs: Any...) {
+        print("ℹ️ ", terminator: "")
+        for (i, x) in xs.enumerated() {
+            print(x, terminator: "")
+
+            if i < xs.endIndex - 1 {
+                print(" ", terminator: "")
+            }
+        }
+        print("")
+    }
+}
